eed8de128f042e48ab717a2fd4756ed7d5fa9fee
Pradeep <pradeep99909@gmail.com>
2020-06-22 00:05:56 +0530

    adding files

---
+++ index.js
@@ -0,0 +1,13 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Initial row ? ", function (val) {
+  val = val.split(" ");
+  var player_1 = [];
+  var player_2 = [];
+  rl.close();
+});
