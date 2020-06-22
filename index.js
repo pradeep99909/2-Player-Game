fa30a1796d6f2b297c48a314a1ad59cbeaed2c03
Pradeep <pradeep99909@gmail.com>
2020-06-22 15:38:00 +0530

    coonecting min max

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const readline = require("readline");
-
+const minimax = require("./minmax");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
 rl.question("Initial row ? ", function (val) {
-  val = val.split(" ");
-  var player_1 = [];
-  var player_2 = [];
+  val = val.split(" ").map((d) => {
+    return Number(d);
+  });
+  var player_A = [];
+  var player_B = [];
+  while (val.length !== 1) {
+    let minmax = minimax(0, 0, true, val, Math.floor(val.length / 2) - 1);
+    val = [
+      ...val.slice(0, val.indexOf(minmax)),
+      ...val.slice(val.indexOf(minmax) + 1, val.length),
+    ];
+  }
   rl.close();
 });
