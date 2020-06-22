ee311029e8066defdafbd4e64c744e2d1ddf0015
Pradeep <pradeep99909@gmail.com>
2020-06-22 14:41:59 +0530

    minimax algo added

---
+++ minmax.js
@@ -0,0 +1,20 @@
+const minimax = (curDepth, nodeIndex, maxTurn, scores, targetDepth) => {
+  // base case  targetDepth reached
+  if (curDepth == targetDepth) {
+    return scores[nodeIndex];
+  }
+
+  if (maxTurn) {
+    return Math.max(
+      minimax(curDepth + 1, nodeIndex * 2, false, scores, targetDepth),
+      minimax(curDepth + 1, nodeIndex * 2 + 1, false, scores, targetDepth)
+    );
+  } else {
+    return Math.min(
+      minimax(curDepth + 1, nodeIndex * 2, true, scores, targetDepth),
+      minimax(curDepth + 1, nodeIndex * 2 + 1, true, scores, targetDepth)
+    );
+  }
+};
+
+module.exports = minimax;
